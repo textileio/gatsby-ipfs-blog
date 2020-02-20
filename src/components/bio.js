6bbd32125f4ab2a51d944194499a491504ac0231
andrew <andrew@textile.io>
2020-02-19 19:48:19 -0700

    update bio
    Signed-off-by: andrew <andrew@textile.io>

--- src/components/bio.js
+++ src/components/bio.js
@@ -54,11 +54,10 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author}</strong> who lives and works in San
-        Francisco building useful things.
+        Written by <strong>{author}</strong> who make tools to build things with IPFS.
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+          You should follow them on Twitter
         </a>
       </p>
     </div>
